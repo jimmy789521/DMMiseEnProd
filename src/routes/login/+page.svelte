c6d773efff9c0b45c06c30576cffb588d9b3ad4a
Galaxycorn <juliann.gaillard@hotmail.fr>
2022-11-03 14:12:02 +0100

    Exemple fetch sveltekit

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from "svelte";
+
+/**
+     * @type {any[]}
+     */
+let users = [];
+
+onMount(async () => {
+  const res = await fetch(`http://localhost:8080/api/users`);
+   users = await res.json();
+});
+
+</script>
+
+<h1> Page test login</h1>
+
+<form method="POST">
+    <input name="username" type="username" placeholder="Nom d'utilisateur">
+    <input name="password" type="password" placeholder="Mot de passe">
+    <button>Log in</button>
+  </form>
+
+  <div>
+    <h1>Users List</h1>
+    {#each users as user}
+      <ul>
+         <li>{user.name}</li>
+     </ul>
+   {:else}
+     <!-- this block renders when users.length === 0 -->
+      <p>loading...</p>
+   {/each}
+</div>
